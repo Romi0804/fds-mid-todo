d52b3bb1c70c74cc9ce7d91211d46c5feda72d38
Ham Choromi <choromi0804@gmail.com>
2018-11-05 15:20:27 +0900

    login form  표시

--- src/index.js
+++ src/index.js
@@ -1,5 +1,26 @@
 import "@babel/polyfill"; // 이 라인을 지우지 말아주세요!
 
-document.querySelector('h1').addEventListener('click', async e => {
-  alert('Hello World!');
-});
+const templates = {
+  loginForm: document.querySelector('#login-form').content
+}
+
+const rootEl = document.querySelector('.root')
+
+function drawLoginForm() {
+ // 1.템플릿 복사하기
+const fragment = document.importNode(templates.loginForm, true)
+
+ // 2. 내용 채우고, 이벤트 리스너 등록하기
+ const loginFormEl = fragment.querySelector('.login-form')
+
+ loginFormEl.addEventListener('submit', async e => {
+   alert('로그인 요청 전송')
+   e.preventDefault()
+ })
+ //개발할때는 그려지는 것부터 확인한 뒤에 기능을 붙여라 !
+
+ // 3. 문서내부에 삽입하기
+ rootEl.appendChild(fragment)
+}
+
+drawLoginForm()
